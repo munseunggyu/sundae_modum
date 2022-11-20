3de695007e50b56531989d26ab938272a33b5b8e
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2022-11-20 16:38:18 +0900

    Docs: PostList style 분리

--- src/components/Post/PostList/index.jsx
+++ src/components/Post/PostList/index.jsx
@@ -5,71 +5,20 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import { doc, onSnapshot, setDoc } from 'firebase/firestore';
 import { db } from '../../../firebase';
-import {
-  clearCurrentPost,
-  setCurrentPost,
-} from '../../../redux/actions/post_action';
 import partyUser from '../../../assets/icons/icon-user.png';
+import {
+  PartyContainer,
+  PartyUser,
+  PostBtn,
+  PostContentContainer,
+  PostImg,
+  Postli,
+  PostTextBottomContainer,
+  PostTextContainer,
+  UserName,
+  UserProfileImg,
+} from './style';
 
-const Postli = styled.li`
-  border-bottom: 1px solid #dbdbdb;
-  margin-bottom: 5px;
-  &:first-child {
-    /* margin-top:5px; */
-  }
-  &:last-child {
-    border: 0;
-  }
-`;
-
-const PostBtn = styled.button`
-  width: 100%;
-  padding-bottom: 5px;
-  background-color: transparent;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-const PostContentContainer = styled.div`
-  display: flex;
-  align-items: flex-end;
-`;
-const UserProfileImg = styled.img`
-  width: 42px;
-  height: 42px;
-  background-color: transparent;
-  border-radius: 50%;
-  margin: 10px 10px 10px 0;
-`;
-const PostImg = styled.img`
-  width: 60px;
-  height: 60px;
-  border-radius: 20px;
-  overflow: hidden;
-`;
-const PostTextContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 2px;
-`;
-const UserName = styled.span`
-  font-weight: 600;
-  font-size: 15px;
-`;
-const PostTextBottomContainer = styled.div`
-  display: flex;
-  gap: 3px;
-  span {
-  }
-`;
-const PartyContainer = styled.div`
-  display: flex;
-`;
-const PartyUser = styled.img`
-  width: 14px;
-  height: 14px;
-`;
 function PostList({
   party,
   postkey,
