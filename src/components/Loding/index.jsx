1694b42d4b9ffa17b32ede12e6d492e9d4d73e96
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2022-11-22 17:16:26 +0900

    Docs: Loding style 분리

--- src/components/Loding/index.jsx
+++ src/components/Loding/index.jsx
@@ -1,53 +1,20 @@
 import { useState } from 'react';
-import logo from '../../assets/logo.png'
-import styled, { keyframes } from 'styled-components';
+import logo from '../../assets/logo.png';
+import { LodingConatiner, LogoImg } from './style';
 
-const fadeInOut = keyframes`
-0% {
-  opacity: 0;
-  transform: translateY(80px);
-}
-100% {
-  opacity: 1;
-  transform: translateY(0);
-}
-`;
-
-const LodingConatiner = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  background-color:${props => props.theme.mainColor};
-  display: ${props => props.display};
-  justify-content: center;
-  align-items: center;
-  z-index: 20;
-  img {
-    animation: ${fadeInOut} 0.5s linear forwards;
-  }
-`;
-const LogoImg = styled.img`
-  width:200px;
-  height:200px;
-  border-radius:10px;
-`;
-
-
-function LodingPage(){
-  const [display, setDisplay] = useState('flex');
-  const hi = () => {
-    setTimeout(() => {
-      setDisplay('none');
-    }, 1500);
-  }
-  hi()
-  return(
-    <LodingConatiner display={display}>
-      <LogoImg src={logo} alt="" />
-    </LodingConatiner>
-  )
+function LodingPage() {
+    const [display, setDisplay] = useState('flex');
+    const hi = () => {
+        setTimeout(() => {
+            setDisplay('none');
+        }, 1500);
+    };
+    hi();
+    return (
+        <LodingConatiner display={display}>
+            <LogoImg src={logo} alt="" />
+        </LodingConatiner>
+    );
 }
 
-export default LodingPage
+export default LodingPage;
---
+++ src/components/Loding/style.js
@@ -0,0 +1,33 @@
+import styled, { keyframes } from 'styled-components';
+
+const fadeInOut = keyframes`
+0% {
+  opacity: 0;
+  transform: translateY(80px);
+}
+100% {
+  opacity: 1;
+  transform: translateY(0);
+}
+`;
+
+export const LodingConatiner = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: ${(props) => props.theme.mainColor};
+    display: ${(props) => props.display};
+    justify-content: center;
+    align-items: center;
+    z-index: 20;
+    img {
+        animation: ${fadeInOut} 0.5s linear forwards;
+    }
+`;
+export const LogoImg = styled.img`
+    width: 200px;
+    height: 200px;
+    border-radius: 10px;
+`;
